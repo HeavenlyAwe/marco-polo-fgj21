22abb76de0c155e23f5e6b6f67c0c34d9ea7d24c
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2021-01-30 11:56:39 +0200

    Multiple sound clips per pickup

--- Assets/PlaySoundClip.cs
+++ Assets/PlaySoundClip.cs
@@ -2,28 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class PlaySoundClip : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public AudioSource[] audioSources;
 
-    //Play the music
-    bool m_Play;
-    //Detect when you use the toggle, ensures music isn’t played multiple times
-    bool m_ToggleChange;
-
+    private int currentAudioSourceIndex = -1;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        //Ensure the toggle is set to true for the music to play at start-up
-        m_Play = true;
+        currentAudioSourceIndex = audioSources.Length - 1;
     }
 
     public void PlaySound(string source)
     {
-        if (audioSource.isPlaying) return;
-
-        audioSource.Play();
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            if (audioSources[i].isPlaying) return;
+        }
+        
+        audioSources[currentAudioSourceIndex].Play();
     }
 }
