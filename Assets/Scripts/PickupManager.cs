378dba56aace83724558e88e8e34b836ea45ccec
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2021-01-31 01:47:38 +0200

    Pickup sound

--- Assets/Scripts/PickupManager.cs
+++ Assets/Scripts/PickupManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PickupManager : MonoBehaviour
 {
     public int minimumPickups = 1;
@@ -14,12 +15,16 @@ public class PickupManager : MonoBehaviour
 
     private GameObject ship = null;
 
+    private AudioSource pickupSoundSource;
+
     // Start is called before the first frame update
     void Awake()
     {
         GameObject[] pickups = GameObject.FindGameObjectsWithTag("Pickup");
         availablePickups = pickups.Length;
         minimumPickups = Mathf.Min(minimumPickups, availablePickups);
+
+        pickupSoundSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -35,6 +40,8 @@ public class PickupManager : MonoBehaviour
 
     public void OnPickup()
     {
+        pickupSoundSource.Play();
+
         collectedPickups += 1;
         if (collectedPickups >= minimumPickups)
         {
