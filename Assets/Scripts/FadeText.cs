3d3d631a89010db22943545064ee98f5f2aaf07b
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2021-01-31 07:16:50 +0200

    Updating the parameters of the sinking ships.

--- Assets/Scripts/FadeText.cs
+++ Assets/Scripts/FadeText.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class FadeText : MonoBehaviour
 {
+    public float waitStartDuration = 2.0f;
     public float fadeInDuration = 10.0f;
     public float fadeOutDuration = 10.0f;
     public float waitDuration = 3.0f;
@@ -18,29 +19,32 @@ public class FadeText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();    
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fadeInDone && fadeOutDuration < 0) return;
-
-        float targetValue = fadeInDone ? 0.0f : 1.0f;
-        float duration = fadeInDone ? fadeOutDuration : fadeInDuration;
-
-        Color color = spriteRenderer.color;
-        color.a = Mathf.Lerp(color.a, targetValue, timer/duration);
-        spriteRenderer.color = color;
-
-        if (color.a >= 0.99f && fadeInDone == false)
+        if (timer >= waitStartDuration)
         {
-            Debug.Log(color.a);
+            if (fadeInDone && fadeOutDuration < 0) return;
 
-            if (timer >= waitDuration)
+            float targetValue = fadeInDone ? 0.0f : 1.0f;
+            float duration = fadeInDone ? fadeOutDuration : fadeInDuration;
+
+            Color color = spriteRenderer.color;
+            color.a = Mathf.Lerp(color.a, targetValue, timer / duration);
+            spriteRenderer.color = color;
+
+            if (color.a >= 0.99f && fadeInDone == false)
             {
-                fadeInDone = true;
-                timer = 0.0f;
+                Debug.Log(color.a);
+
+                if (timer >= waitDuration + waitStartDuration)
+                {
+                    fadeInDone = true;
+                    timer = 0.0f;
+                }
             }
         }
 
